e92d7a65450952a227791c1dab505afd8acab621
Albina Anistratenko <109683293+albuska@users.noreply.github.com>
2023-11-05 17:19:31 +0200

    Update listOfBestPlayers.js

--- controllers/games/listOfBestPlayers.js
+++ controllers/games/listOfBestPlayers.js
@@ -2,13 +2,13 @@ const db = require("../../db");
 const { ctrlWrapper } = require("../../helpers");
 
 const listOfBestPlayers = async (req, res) => {
-  const list = await db.query(
+  const {rows: players} = await db.query(
     `SELECT player_name, score FROM games ORDER BY score DESC`
   )  
 
-  console.log("list", list);
+  console.log("list", players);
 
-  res.status(200).json({ list });
+  res.status(200).json({ players });
 }
 
 module.exports = {
